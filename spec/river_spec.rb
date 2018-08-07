faa10dd00ee9a8ddde22d0ed4d063f429bab5f22
nimbus117 <mail@nimbus117.co.uk>
2018-08-07 17:36:23 +0100

    added river has name/fishes tests

---
+++ spec/river_spec.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../river.rb')
+require_relative('../fish.rb')
+
+class FishTest < MiniTest::Test
+  def setup
+    fish1 = Fish.new('nemo')
+    fish2 = Fish.new('ponyo')
+    fish3 = Fish.new('dory')
+    fishes = [fish1, fish2, fish3]
+    @river = River.new('Amazon', fishes)
+  end
+
+  def test_river_has_name
+    assert_equal('Amazon', @river.name)
+  end
+
+  def test_river_has_fish
+    assert_equal(3, @river.fishes().length())
+  end
+end
