bf13d8855d30c423844cccd99514d8abf84d932e
nimbus117 <mail@nimbus117.co.uk>
2018-08-07 17:48:19 +0100

    passed fish_count test

--- river.rb
+++ river.rb
@@ -2,8 +2,12 @@ class River
 
   attr_reader(:name, :fishes)
 
-  def initialize(name, fishes)
+  def initialize(name, fishes = [])
     @name = name
     @fishes = fishes
+  end
+
+  def fish_count
+    return @fishes.length
   end
 end
